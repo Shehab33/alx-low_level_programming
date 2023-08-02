865210714d706eefce8ddec50d9a0fc03cf914d4
Shehab Mohammed <shehabjaheen@gmail.com>
2023-08-02 08:30:31 -0700

    test1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _sqrt_recursion - This is my first function
+ * @n: This is my value
+ *
+ * Return: This is my result of the function my_sqrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	return (my_sqrt_recursion(n, 0));
+}
