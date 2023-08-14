2543886adcd0ccff7349b500542777a81ce7d85e
Shehab33 <38076648+Shehab33@users.noreply.github.com>
2023-08-14 14:30:27 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,12 @@
-
+/*
+* struct dog - struct that has dog info
+* @name: dog name
+* @age: dog age in float
+* @owner: dog owner name
+* Description: it is a normal dog struct
+*/
+struct dog {
+	char* name;
+	float age;
+  char* owner;
+};
